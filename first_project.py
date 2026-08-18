2494a85d6991ae0d59e03b4fe8275580701d71b9
TheBattlesmith <roman.torres@ucas-edu.net>
2026-08-18 09:12:01 -0600

    first commit

---
+++ first_project.py
@@ -0,0 +1,16 @@
+#Roman Torres, First Project
+
+print("Hello, world")#this is an output
+
+print("this is programing 1")
+
+#input is what is being retrieved by the program
+#inputs must be saved in variables 
+name = input("what is your name: ")
+
+print("welcome to class " + name)
+
+print("What did you do this weekend: ")
+
+weekend = input()
+print("that's super cool I wish I " + weekend+ " this weekend.")
