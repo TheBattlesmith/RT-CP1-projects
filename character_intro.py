e07a414cdfb8b907e1361ab9f2f3ebcb5dee6d07
TheBattlesmith <roman.torres@ucas-edu.net>
2026-08-20 09:46:14 -0600

    I adjusted a variable name

--- character_intro.py
+++ character_intro.py
@@ -1,12 +1,12 @@
 # Roman Torres, character introduction
 
-name = "Alexander"
+alexName = "Alexander"
 age = 18
 job = "cyber forensics expert"
 home = "Salt Lake City"
 
 
-print("Hi, my name is, " + name) 
+print("Hi, my name is, " + alexName) 
 print("I am, ", age, "years old.")
 print("I work as an, " + job)
 print("And I'm from, " + home)
