be657dc3e4631d1fd08a7fd3cda311a305ad6ea7
TheBattlesmith <roman.torres@ucas-edu.net>
2026-08-20 09:43:42 -0600

    this is the character intro

---
+++ character_intro.py
@@ -0,0 +1,12 @@
+# Roman Torres, character introduction
+
+name = "Alexander"
+age = 18
+job = "cyber forensics expert"
+home = "Salt Lake City"
+
+
+print("Hi, my name is, " + name) 
+print("I am, ", age, "years old.")
+print("I work as an, " + job)
+print("And I'm from, " + home)
