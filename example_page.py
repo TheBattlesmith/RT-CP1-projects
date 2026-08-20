b716638120b60bf7a390cae045dfb3a1c20d6ad2
TheBattlesmith <roman.torres@ucas-edu.net>
2026-08-20 09:04:55 -0600

    this is an example page for coding concepts

---
+++ example_page.py
@@ -0,0 +1,19 @@
+#Roman Torres Variables notes
+
+name = "Katie"
+
+print("hello "+ name)
+
+# reset variable
+
+name ="peter"
+
+print("Sorry I meant, "+ name)
+
+#another variable example
+age = 16
+
+print("Wow you are, ", age)
+print("Oh wait your birthday was yesterday?")
+age += 1
+print("so you are, ", age)
