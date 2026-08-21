d78fcef1b3dd4652f85e6ea062ac5fbb65bdc959
TheBattlesmith <roman.torres@ucas-edu.net>
2026-08-21 09:52:09 -0600

    I added examples

--- example_page.py
+++ example_page.py
@@ -19,3 +19,24 @@ age += 1
 print("so you are, ", age)
 
 print("we did this on codespace")
+
+# debugging is the process of removing errors from your code
+
+#error types: 
+# Syntax errors - you wrote it wrong, indentation error
+# Logic errors - program runs but produces wrong result, because your steps are wrong
+# Run time Errors - errors that happen when the code is running causing them to crash partway through
+    #example
+#fav_num = int(input("what is your favorite number: "))
+#print(4 + fav_num)
+    #the fix
+while True:
+    try:
+        fav_num = input("what is your favorite number: ")
+    except:
+        print("that's not a number")
+    else:
+        break
+print(4 + fav_num)
+        
+        
