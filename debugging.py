e6294a6f9ce2f707ab164caa4c1f875bd839002e
TheBattlesmith <roman.torres@ucas-edu.net>
2026-08-24 09:13:28 -0600

    debugging file

---
+++ debugging.py
@@ -0,0 +1,10 @@
+# Roman Torres, fix the code
+
+name = "Alex"
+age = 16
+
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+      
+#the print statement needed a closing parenthese so I added one
+# name in the print statement was mispelled, it must match the variable
