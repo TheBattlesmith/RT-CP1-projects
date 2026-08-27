6a006423360598109d9391bd956f6861f6bd667f
TheBattlesmith <roman.torres@ucas-edu.net>
2026-08-27 09:26:02 -0600

    i added the introducton

--- unit1_final_project.py
+++ unit1_final_project.py
@@ -2,7 +2,7 @@
 
 name = input("Hello there, what is your name? ")
 
-hobby = input(f"nice to meet you, {name}. what do you enjoy to do? ")
+hobby = input(f"nice to meet you, {name}. what do you enjoy doing? ")
 
 home = input(f"{hobby}, eh, can't say I've ever done that before. Where do you live? ")
 
@@ -28,3 +28,5 @@ if age <= 20:
     print("okay, so not too old then. ")
 if age < 100 and age > 20:
     print("Ah,so you're an adult then")
+
+print(f"Oh hey, here comes my friend, Dave. Hey, Dave, meet {name}, they live in a place called {home}, and are {age} years old, and get this, you both enjoy {hobby} ")
