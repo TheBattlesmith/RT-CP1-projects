6a23375e420b1ac4749a1650aec242e309220a4b
TheBattlesmith <roman.torres@ucas-edu.net>
2026-09-08 18:53:38 +0000

    more code added

--- idiot_proof.py
+++ idiot_proof.py
@@ -1,5 +1,6 @@
 # Roman Torres, idiot proof assignment
 
+#name
 
 first_name = input("What is your first name? ").strip().title()
 last_name = input("What is your last name? ").strip().title()
@@ -11,6 +12,7 @@ seperate_two = last_name.split()
 fixed_firstname = "".join(seperate_one).strip().title()
 fixed_lastname = "".join(seperate_two).strip().title()
 
+#phone number
 
 while True:
 
@@ -18,20 +20,39 @@ while True:
         phone_number = (int(input("What is your phone number? ")))
 
     except:
-        print("Please enter a valid phone number")
+        print("Please enter a valid phone number with no spaces")
 
     else:
         break
 
 
+string_phone = str(phone_number)
+
+
+first_phone = string_phone[0:3]
+second_phone = string_phone[3:6]
+third_phone = string_phone[6:10]
+
+final_phone = first_phone + " " + second_phone + " " + third_phone
+
+#GPA
+
+while True:
+    try:
+        gpa_question = int(input("What is your GPA? "))
+    except:
+        print("That is not a valid GPA.")
+    else:
+        break
+
+
+GPA = (round(gpa_question, 2))
 
 
 
 
-
-
-
-
-
+print(final_phone)
 
 print(fixed_firstname + " " + fixed_lastname)
+
+print(GPA)
