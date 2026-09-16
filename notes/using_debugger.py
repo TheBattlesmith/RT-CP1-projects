944db9218f6153597da274a18558563b308bd1d4
TheBattlesmith <roman.torres@ucas-edu.net>
2026-09-16 09:44:51 -0600

    debugger examples

--- notes/using_debugger.py
+++ notes/using_debugger.py
@@ -1,16 +1,32 @@
 # Debugger notes 
 
-
 # VL, Using the debugger
 
+
+'''
 grades = [85, 90, 78, 92, 88]
 
 total = 0
-count = 4
+count = len(grades)
 
 for grade in grades:
     total = total + grade
 
 average = total / count
 
-print(f"the average grade is {average}")
+print(f"the average grade is {average}")'''
+
+
+scores = [12, 45 , 7 , 68, 33, 90, 21]
+
+running_total = 0
+highest_score = 0
+
+for score in scores:
+    running_total += score 
+    if score > highest_score:
+        highest_score = score
+
+
+print(f"Total: {running_total}")
+print(f"Highest score: {highest_score}")
