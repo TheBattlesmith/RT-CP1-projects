d8fc600af63ecc726481115aeb4c6ecae882ce5b
TheBattlesmith <roman.torres@ucas-edu.net>
2026-09-16 09:10:11 -0600

    debugger notes

---
+++ notes/using_debugger.py
@@ -0,0 +1,16 @@
+# Debugger notes 
+
+
+# VL, Using the debugger
+
+grades = [85, 90, 78, 92, 88]
+
+total = 0
+count = 4
+
+for grade in grades:
+    total = total + grade
+
+average = total / count
+
+print(f"the average grade is {average}")
