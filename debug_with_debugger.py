18c30095c0d7b142e57a408d308f49d77a2ef71f
TheBattlesmith <roman.torres@ucas-edu.net>
2026-09-17 09:26:11 -0600

    debug with debugger finished first revision

--- debug_with_debugger.py
+++ debug_with_debugger.py
@@ -9,17 +9,17 @@ pirate_name = input("What's your name, pirate? ")
 snack_name = input("What snack do you want? ")
 
 price = random.randint(2, 8)  # random price in credits
-quantity = input("How many would you like? ")
+quantity = int(input("How many would you like? ")) # I changed quantity to int
 
-total = price * quantity
+total = int(price * quantity)
 
-discounted_total = total - 2 * 0.10
+discounted_total = total - (total * 0.10) # I changed the equation to actually match what a ten percent discount would look like
 
 tax_rate = 0.08
 total_with_tax = discounted_total + (discounted_total * tax_rate)
 
 print("Hello, " + pirate_name + "! Here's your order summary:")
-print("Snack: " + snackName)
+print("Snack: " + snack_name) # I changed SnackName to match the variable located on line 9
 print("Price per snack: " + str(price) + " credits")
-print("Total before tax: " + str(price))
+print("Total before tax: " + str(discounted_total)) # I changed price to discounted total in order to get the total price before tax
 print("Total with tax: " + str(round(total_with_tax, 2)) + " credits") # parenthese was missing
